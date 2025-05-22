ea533d7f7c989e2155fa12d6779f0794abe009a3
Cyberarm <matthewlikesrobots@gmail.com>
2025-05-21 21:18:57 -0500

    Stub networking stuff

--- include/cone_light_networking.h
+++ include/cone_light_networking.h
@@ -8,20 +8,23 @@
 // Forward declaration...
 class ConeLight;
 
+typedef struct cone_light_network_packet
+{
+  uint8_t peer_id;
+} cone_light_network_packet_t;
+
 class ConeLightNetworking
 {
 private:
   ConeLight *m_cone_light = nullptr;
+  uint8_t m_broadcast_address[6] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
 
 public:
   ConeLightNetworking(ConeLight *cone_light);
   ~ConeLightNetworking();
   void update();
+  void send_packet(const uint8_t *mac_addr, cone_light_network_packet_t packet);
+  void broadcast_packet(cone_light_network_packet_t packet);
   void on_data_sent(const uint8_t *mac_addr, esp_now_send_status_t status);
-  void on_data_received(const uint8_t *mac_addr, const uint8_t *incomingData, int len);
+  void on_data_received(const uint8_t *mac_addr, const uint8_t *data, int len);
 };
-
-typedef struct cone_light_network_packet
-{
-  uint8_t peer_id;
-} cone_light_network_packet_t;
--- src/cone_light_networking.cpp
+++ src/cone_light_networking.cpp
@@ -22,10 +22,24 @@ void ConeLightNetworking::update()
 {
 }
 
+void ConeLightNetworking::send_packet(const uint8_t *mac_addr, cone_light_network_packet_t packet)
+{
+  esp_now_send(mac_addr, (uint8_t *)&packet, sizeof(packet));
+}
+
+void ConeLightNetworking::broadcast_packet(cone_light_network_packet_t packet)
+{
+  esp_now_send(m_broadcast_address, (uint8_t *)&packet, sizeof(packet));
+}
+
 void ConeLightNetworking::on_data_sent(const uint8_t *mac_addr, esp_now_send_status_t status)
 {
 }
 
-void ConeLightNetworking::on_data_received(const uint8_t *mac_addr, const uint8_t *incomingData, int len)
+void ConeLightNetworking::on_data_received(const uint8_t *mac_addr, const uint8_t *data, int len)
 {
+  cone_light_network_packet_t packet;
+  memcpy(&packet, data, sizeof(packet));
+
+  // m_cone_light->networking_event(packet);
 }
