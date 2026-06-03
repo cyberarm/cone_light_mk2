c44ab682cd084952f1572b1c9ac5f811ecf4ea95
Cyberarm <matthewlikesrobots@gmail.com>
2026-06-03 09:05:13 -0500

    Added Sweet Hour of Prayer to song catalog

--- include/cone_light_songs.h
+++ include/cone_light_songs.h
@@ -4,7 +4,7 @@
 #include <cstdint>
 #include <WString.h>
 
-extern const std::array<const String, 51> cone_light_song_titles;
-extern const std::array<const std::array<int8_t, 476>, 408> cone_light_song_notes;
-extern const std::array<const std::array<uint16_t, 476>, 408> cone_light_song_durations;
+extern const std::array<const String, 52> cone_light_song_titles;
+extern const std::array<const std::array<int8_t, 476>, 416> cone_light_song_notes;
+extern const std::array<const std::array<uint16_t, 476>, 416> cone_light_song_durations;
 
